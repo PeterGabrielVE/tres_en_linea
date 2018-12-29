ae615d11adcda288579a22aff02237593d3cba97
Pedro Gabriel <gaboleal123@gmail.com>
2018-12-29 18:39:15 -0430

    Instalando Middlewares
    Instalando body_parser, cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3030);
 
